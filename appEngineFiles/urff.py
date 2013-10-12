aa6475ed27308d20e8c68ca7ee6e70841b77327c
BeckyGalasso <rgalasso@u.rochester.edu>
2013-10-12 18:12:55 -0400

    Test html code in urff.py

--- appEngineFiles/urff.py
+++ appEngineFiles/urff.py
@@ -1,4 +1,3 @@
-
 import cgi
 import datetime
 import webapp2
@@ -10,10 +9,13 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
     
     #Insert code the HTML code in the stuff below
-    self.response.out.write("""
-			    
-			    
-			    """
+    self.response.out.write("<html>\n\t<head>\n\t\t<title>Event Details</title>" + 
+    	"\n\t\t\t<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\"/>" +
+	"\n\t\t\t<link href=\"EventDetails.css\" type=\"text/css\" rel=\"stylesheet\"/>" +
+	"\n\t\t\t<link href = \"../rocky.gif\" type = \"image/gif\" rel = \"shortcut icon\" /> +
+	"\n\t</head>\n\n\t<body>" +
+	"\n\t\t<p>This is a test</p>\n\t</body>" +
+	"\n</html>")
 
 
 
